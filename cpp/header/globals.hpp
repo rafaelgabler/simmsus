9c926c1634eb38d53d3ac22012495c1b03bf4f6b
Leandro Zanotto <leandro.zanotto@gmail.com>
2024-07-31 11:50:30 -0300

    Updated

---
+++ cpp/header/globals.hpp
@@ -0,0 +1,29 @@
+/*
+ * This program is licensed granted by the University of Brasília - UnB (the “University”)
+ * for use of SIMULATION OF MAGNETIC SUSPENSIONS - SIMMSUS (“the Software”) through this website
+ * https://github.com/rafaelgabler/simmsus (the ”Website”).
+ *
+ * By downloading the Software through the Website, you (the “Licensee”) are confirming that you agree
+ * that your use of the Software is subject to the academic license terms.
+ *
+ * For more information about SIMMSUS please contact: rafael.gabler@unb.br (Rafael Gabler Gontijo)
+ * or leandro.zanotto@gmail.com (Leandro Zanotto).
+ *
+ */
+
+#ifndef SRC_HEADERS_GLOBALS_HPP_
+#define SRC_HEADERS_GLOBALS_HPP_
+#include <math.h>
+
+extern int nnr;
+extern int npast;
+extern int numRealizations;
+extern int numParticles;
+extern int num;
+extern double l;
+extern double h;
+extern double *diarand;
+extern int totalRealParticle;
+
+
+#endif /* SRC_HEADERS_GLOBALS_HPP_ */
